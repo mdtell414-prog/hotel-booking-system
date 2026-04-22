6d9ad1555074f0ffab9993726dca6f8a334d93ec
Mohamed Ahmed <md.tell414@gmail.com>
2026-04-22 23:29:02 +0200

    use memory instead of file

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require("express");
-const fs = require("fs");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(__dirname));
 
-/* تأكد أن الملف موجود */
-if (!fs.existsSync("bookings.json")) {
-  fs.writeFileSync("bookings.json", "[]");
-}
+/* تخزين مؤقت */
+let bookings = [];
 
 /* إضافة حجز */
 app.post("/boka", (req, res) => {
@@ -19,36 +15,18 @@ app.post("/boka", (req, res) => {
 
   const booking = { name, email, date };
 
-  let bookings = [];
-
-  try {
-    bookings = JSON.parse(fs.readFileSync("bookings.json"));
-  } catch {
-    bookings = [];
-  }
-
   bookings.push(booking);
-
-  fs.writeFileSync("bookings.json", JSON.stringify(bookings, null, 2));
 
   console.log("Ny bokning:", booking);
 
   res.redirect("/success.html");
 });
 
-/* جلب الحجوزات */
+/* عرض الحجوزات */
 app.get("/bookings", (req, res) => {
-  let bookings = [];
-
-  try {
-    bookings = JSON.parse(fs.readFileSync("bookings.json"));
-  } catch {
-    bookings = [];
-  }
-
   res.json(bookings);
 });
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
-});
+})
