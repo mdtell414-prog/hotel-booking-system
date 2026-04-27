6acce7523757a1d903a8ddbd46e78ad4c323dfda
Mohamed Ahmed <md.tell414@gmail.com>
2026-04-27 08:45:49 +0200

    connect mongodb

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -6,24 +7,41 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
-let bookings = [];
 
-app.post("/boka", (req, res) => {
-  const { name, email, date } = req.body;
+mongodb+srv;//tell2000wdalkar_adminuser414ahmed-710:Wdalkar.tell2000!@cluster0.xcjwhoh.mongodb.net/?appName=Cluster0
+mongoose.connect("PUT_YOUR_MONGODB_LINK_HERE")
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
 
-  const booking = { name, email, date };
 
-  bookings.push(booking);
-
-  console.log("Ny bokning:", bookings);
-
-  res.send("Booking mottagen!");
+// Schema
+const bookingSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  date: String
 });
 
-app.get("/bookings", (req, res) => {
+const Booking = mongoose.model("Booking", bookingSchema);
+
+
+// POST (حجز)
+app.post("/boka", async (req, res) => {
+  const { name, email, date } = req.body;
+
+  const booking = new Booking({ name, email, date });
+  await booking.save();
+
+  res.send("Bokning mottagen");
+});
+
+
+// GET (عرض الحجوزات)
+app.get("/bookings", async (req, res) => {
+  const bookings = await Booking.find();
   res.json(bookings);
 });
 
+
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
-})
+});
